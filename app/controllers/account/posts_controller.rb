2d07bd342ff050fad3bc685664ac29f9d6d52363
suilulu <m13869841793@gmail.com>
2017-03-26 22:21:14 +0800

    use simple_format helper to decorate

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,36 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
+
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @posts = current_user.posts
+    @post = Post.find(params[:group_id])
+  end
+
+  def update
+    @posts = current_user.posts
+    @post = Post.find(params[:group_id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @posts = Post.destroy
+    redirect_to posts_path, alert: "Deleted success".
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
 end
